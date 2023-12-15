cff58dbe0365c0d7f9312ec39d7992c3de9c4576
HDTKhoa <huynhtongdangkhoa@gmail.com>
2023-12-15 20:26:47 +0700

    Sua lai file + add them comment

--- lab6.c
+++ lab6.c
@@ -26,80 +26,20 @@ void addToHistory(char *command) {
 }
 
 void printHistory() {
-    printf("Command History:\n");
-    for (int i = 0; i < MAX_HISTORY; i++) {
-        if (history[i] != NULL) {
-            printf("%d: %s\n", i + 1, history[i]);
-        }
-    }
 }
 
+
 void executeCommand(char *command) {
-    // Check for pipe symbol
-    char *pipeToken = strchr(command, '|');
-
-    if (pipeToken != NULL) {
-        // If pipe symbol is found
-        char *command1 = strtok(command, "|");
-        char *command2 = strtok(NULL, "|");
-
-        // Remove leading and trailing spaces
-        command1 = strtok(command1, " \t\n\r");
-        command2 = strtok(command2, " \t\n\r");
-
-        // Create a pipe
-        int pipefd[2];
-        if (pipefpipe(d) == -1) {
-            perror("Pipe creation failed");
-            exit(EXIT_FAILURE);
-        }
-
-        // Fork the first child process
-        pid_t pid1 = fork();
-
-        if (pid1 == -1) {
-            perror("Fork failed");
-            exit(EXIT_FAILURE);
-        } else if (pid1 == 0) {
-            // First child process
-
-            // Redirect stdout to the write end of the pipe
-            dup2(pipefd[1], STDOUT_FILENO);
-            close(pipefd[0]);
-            close(pipefd[1]);
-
-            // Execute the first command
-            execlp(command1, command1, NULL);
-            perror("Exec failed");
-            exit(EXIT_FAILURE);
-        }
-
-        // Parent process
-        close(pipefd[1]);
-
-        // Wait for the first child process to complete
-        waitpid(pid1, NULL, 0);
-
-        // Redirect stdin from the read end of the pipe
-        dup2(pipefd[0], STDIN_FILENO);
-        close(pipefd[0]);
-
-        // Tokenize and execute the second command
-        execlp(command2, command2, NULL);
-        perror("Exec failed");
-        exit(EXIT_FAILURE);
-    }
-else {
-    // Fork a child process
+    // Bai 1
+    // Fork 1 tien trinh con
     pid_t pid = fork();
 
     if (pid == -1) {
         perror("Fork failed");
         exit(EXIT_FAILURE);
     } else if (pid == 0) {
-        // Child process
-
-        // Tokenize the command
+        // Process cua tien trinh con
+        // Tach command ra thanh nhieu thanh phan qua dau " "
         char *arguments[MAX_LINE / 2 + 1];
         char *token = strtok(command, " ");
         int i = 0;
@@ -109,9 +49,9 @@ else {
             token = strtok(NULL, " ");
         }
 
-        arguments[i] = NULL; // Null-terminate the argument list
+        arguments[i] = NULL; // Null-terminate mang arguments
         
-        // Check for redirection
+        // Bai 3 Check toan tu chuyen huong vao ra
         int redirectOutput = 0;
         int redirectInput = 0;
 
@@ -124,7 +64,7 @@ else {
         }
 
         if (redirectOutput > 0) {
-            // Redirect output to a file
+            // Toan tu chuyen huong dau vao mot file ('>')
             /*
             The O_WRONLY flag --> opened for writing only.
             The O_CREAT flag --> created a file if it doesn't exist.
@@ -146,7 +86,7 @@ else {
         }
 
         if (redirectInput > 0) {
-            // Redirect input from a file
+            // Toan tu chuyen huong dau ra tu mot file ('<')
             int fd = open(arguments[redirectInput + 1], O_RDONLY);
             if (fd == -1) {
                 perror("Open failed");
@@ -162,7 +102,7 @@ else {
             arguments[i - 2] = NULL;
         }
        
-        // Execute the command
+        // Thuc thi cau lenh
         if (strcmp(command, "HF") == 0) {
             printHistory(command);
         } else {
@@ -175,17 +115,14 @@ else {
     } 
     else {
         // Parent process
-
         // Wait for the child to finish
         waitpid(pid, NULL, 0);
     }
 }
 
-}
-
 int main(void) {
     char command[MAX_LINE/ 2 + 1];
-    int should_run = 1; /* flag to determine when to exit program */
+    int should_run = 1; /* flag de biet khi nao ket thuc chuong trinh */
 
     while (should_run) {
         printf("it007sh> ");
@@ -196,12 +133,12 @@ int main(void) {
             break;
         }
 
-        // Check for exit command
+        // Check exit command
         if (strcmp(command, "exit") == 0) {
             break;
         }
 
-        // Remove last character ("\n")
+        // Xoa phan tu cuoi ("\n")
         size_t len = strlen(command);
         if (len > 0 && command[len - 1] == '\n') {
             command[len - 1] = '\0';
